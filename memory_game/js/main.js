434374828f752291f2569ce25881bf8f4ee8c85f
William Winston <winston@Williams-Air.home>
2020-01-11 13:51:10 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardTwo);
