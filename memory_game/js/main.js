70753129579bd0a5af0c617225de0760ac5e1a98
William Winston <winston.william@gmail.com>
2020-01-12 13:16:03 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,52 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardId = [];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
 
+}
+];
+//Monitor cards in play
+var cardsInPlay = [];
+//Check for a card match
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!"); 
+		alert("You found a match!"); 
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
-
+//flip card and check pair
 var flipCard = function(cardId) {
-	console.log("User flipped" + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay.length === 2){
+	console.log("User flipped" + cards[cardId].rank);
+		cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+}
+	
+if (cardsInPlay.length === 2){ 
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		checkForMatch();
-    }
+	}
 };
 
 flipCard(0);
 flipCard(2);
+checkForMatch();
+
+
