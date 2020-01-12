2895dbb81e877efa389f1ee967dbb605419b2480
William Winston <winston.william@gmail.com>
2020-01-12 03:14:04 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,22 @@
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push('card0ne');
-console.log("User flipped" + " " + cardOne);
-let cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped" + " " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardId = [];
 
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else; {
-	alert("Sorry, try again.");
+		console.log("You found a match!"); 
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
-}
 
+var flipCard = function(cardId) {
+	console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+    }
+};
 
-
+flipCard(0);
+flipCard(2);
