6e579f6b98897412f0a585955241018e266c9005
William Winston <winston.william@gmail.com>
2020-01-12 23:01:48 -0500

    created game logic and dom

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,29 +24,35 @@ const cards = [
 //Monitor cards in play
 var cardsInPlay = [];
 //Check for a card match
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+ 	}
+}
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[1] === cardsInPlay[2]) {
 		alert("You found a match!"); 
 	} else {
 		alert("Sorry, try again.");
 	}
-}
 //flip card and check pair
-var flipCard = function(cardId) {
-	console.log("User flipped" + cards[cardId].rank);
-		cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+			this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped" + cards[cardId].rank + card[cardId].suit);
+	console.log("User flipped" + cards[cardId].cardImage);
+	console.log("User flipped" + cards[cardId].suit);
 }
-	
-if (cardsInPlay.length === 2){ 
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		checkForMatch();
-	}
+		if (cardsInPlay.length === 2); {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+		 checkForMatch();
+		}
 };
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
 
-
+createBoard();
